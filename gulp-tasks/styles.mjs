0eb7ba44b251ac3ead46455c51730267b2054225
dtverdokhlebova <tverdokhlebova.d.a@yandex.ru>
2023-12-05 00:29:25 +0300

    init commit

---
+++ gulp-tasks/styles.mjs
@@ -0,0 +1,39 @@
+import gulp from 'gulp'
+import plumber from 'gulp-plumber'
+import gulpStylelint from 'gulp-stylelint'
+import filter from 'gulp-filter'
+import sassGlob from 'gulp-sass-glob'
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import autoprefixer from 'gulp-autoprefixer'
+import rename from 'gulp-rename'
+import browsersync from 'browser-sync'
+import header from 'gulp-header'
+
+const sass = gulpSass(dartSass)
+
+const headerStyles = `@import '../../styles/utils/variables';\n`
+
+export default function styles() {
+  return gulp.src('src/**/*.scss') // преобразование в css
+    .pipe(plumber())
+    .pipe(gulpStylelint({
+      failAfterError: false,
+      reporters: [{ formatter: 'string', console: true }]
+    }))
+    .pipe(filter(['src/styles/style.scss', 'src/styles/main.scss']))
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(gulp.dest('build'))
+    .pipe(header(headerStyles))
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(gulp.dest('build'))
+    .pipe(gulp.src('src/**/*.css')) // библиотеки
+    .pipe(filter('src/libs/**/*.css'))
+    .pipe(gulp.dest('build'))
+    // .pipe(browsersync.stream())
+    .on('end', browsersync.reload)
+}
